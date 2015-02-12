bb65eb5f23a72b01dd0e72e5f6469d7a0f08cf68
Drew Fleeman <drew.fleeman@gmail.com>
2015-02-12 09:53:06 -0800

    trying to fix heroku footer problem

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module ProjectSkeleton
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+
     config.filepicker_rails.api_key = "ABHEUcLvhRluRSDU5fkBhz"
+    config.assets.initialize_on_precompile = false
   end
 end
